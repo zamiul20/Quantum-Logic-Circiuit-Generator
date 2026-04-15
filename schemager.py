c85f97c9a0586ba310bd705ba6f26a42535a6514
zamiul20 <143951287+zamiul20@users.noreply.github.com>
2026-04-15 19:16:34 +0100

    Update schemager.py

--- schemager.py
+++ schemager.py
@@ -39,6 +39,7 @@ def blipi(t : turtle, gps : int):
     t.setheading(0) ; t.forward(gps / 2)
     t.penup()
 
+#?
 def whynot(t : turtle, gps : int):
     t.pendown()
     t.back(gps / 2) ; t.setheading(270)
@@ -99,6 +100,7 @@ def exclugat(t : turtle, pos1 : int, pos2 : int, end : int, gps : int, start, in
     t.penup()
     return end
 
+#to be or not to be, that is the question
 def tobegat(t : turtle, pos1 : int, pos2 : int, end : int, gps : int, start, inver : int | None = 0, leenlen : float | None = gps) -> float:
     end = andgat(t, pos1, pos2, end, gps, start, 1, leenlen)
     end = exclugat(t, pos1, pos2, end, gps, start, 1, leenlen)
@@ -106,6 +108,7 @@ def tobegat(t : turtle, pos1 : int, pos2 : int, end : int, gps : int, start, inv
 
     return end
 
+#le full adder
 def fadder(t : turtle, pos1 : int, pos2 : int, end : int, gps : int, start, leenlen : float | None = gps) -> float:
     end = exclugat(t, pos1, pos2, end, gps, start, 0, leenlen)
     end = andgat(t, pos1, pos2, end, gps, start, 0, leenlen)
@@ -117,6 +120,7 @@ def fadder(t : turtle, pos1 : int, pos2 : int, end : int, gps : int, start, leen
 
     return end
 
+#le half adder
 def hadder(t : turtle, pos1 : int, pos2 : int, end : int, gps : int, start, leenlen : float | None = gps) -> float:
     end = exclugat(t, pos1, pos2, end, gps, start, 0, leenlen)
     end = andgat(t, pos1, pos2, end, gps, start, 0, leenlen)
@@ -202,148 +206,3 @@ letur.goto(start[0] - 30, start[1] + 10)
 
 if (input("sav ") == "yes"):
     turtle.getcanvas().postscript(file="welp.eps")
-
-start = [-200, 150]
-
-gps = 20
-
-letur.penup()
-letur.goto(start)
-lines = 0
-end = 0
-
-
-
-def makleen(t : turtle, value : any, end : int, gps : int) -> float:
-    t.penup()
-    t.goto(start[0] - 5, start[1] + (end - gps))
-    t.setheading(0)
-    t.write(f"|{value}>", align="right")
-    t.forward(5)
-    t.pendown()
-    x = t.ycor() - start[1]
-    t.forward(gps)
-    t.penup()
-
-    return x
-
-def blipi(t : turtle, gps : int):    
-    t.pendown()
-    t.back(gps / 2) ; t.setheading(270)
-    t.fillcolor('blue') ; t.begin_fill()
-    t.circle(gps / 2) ; t.end_fill()
-    t.setheading(0) ; t.forward(gps / 2)
-    t.penup()
-
-def whynot(t : turtle, gps : int):
-    t.pendown()
-    t.back(gps / 2) ; t.setheading(270)
-    t.circle(gps / 2)
-    t.setheading(0) ; t.forward(gps) ; t.back(gps / 2)
-    t.setheading(270) ; t.forward(gps / 2) ; t.back(gps)
-    t.forward(gps / 2) ; t.setheading(0)
-    t.penup()
-
-def andgat(t : turtle, pos1 : int, pos2 : int, end : int, gps : int, start : list, inver : int | None = 0) -> float:
-    xy = [t.xcor(), t.ycor()]
-    end = makleen(t, inver, end, gps)
-    t.goto(xy)
-
-    t.penup()
-    t.setheading(0)
-    t.forward(gps)
-    t.goto(t.xcor(), (start[1] - (pos1 * gps)))
-    blipi(t, gps / 2)
-    t.pendown()
-
-    t.goto(t.xcor(), (start[1] - (pos2 * gps)))
-    blipi(t, gps / 2)
-    t.pendown()
-
-    t.goto(t.xcor(), end)
-    whynot(t, gps / 2)
-    t.penup()
-
-    return end
-
-def exclugat(t : turtle, pos1 : int, pos2 : int, end : int, gps : int, start, inver : int | None = 0) -> float:
-    xy = [t.xcor(), t.ycor()]
-    end = makleen(t, inver, end, gps)
-    t.goto(xy)
-
-    t.penup()
-    t.setheading(0)
-    t.forward(gps)
-
-    t.goto(t.xcor(), (start[1] - (pos1 * gps)))
-    blipi(t, gps / 2)
-
-    t.pendown()
-    t.goto(t.xcor(), end)
-    whynot(t, gps / 2)
-
-    t.penup()
-    t.forward(gps)
-
-    t.goto(t.xcor(), (start[1] - (pos2 * gps)))
-    blipi(t, gps / 2)
-
-    t.pendown()
-    t.goto(t.xcor(), end)
-    whynot(t, gps / 2)
-
-    t.penup()
-    return end
-
-def tobegat(t : turtle, pos1 : int, pos2 : int, end : int, gps : int, start, inver : int | None = 0) -> float:
-    end = andgat(t, pos1, pos2, end, gps, start, 1)
-    end = exclugat(t, pos1, pos2, end, gps, start, 1)
-    end = andgat(t, -((end / gps) + 1), -(end / gps), end, gps, start, inver)
-
-    return end
-
-'''
-end = makleen(letur, 'A1', end, gps)
-end = makleen(letur, 'A2', end, gps)
-end = makleen(letur, 'A3', end, gps)
-end = makleen(letur, 'A4', end, gps)
-end = makleen(letur, 'B1', end, gps)
-end = makleen(letur, 'B2', end, gps)
-end = makleen(letur, 'B3', end, gps)
-end = makleen(letur, 'B4', end, gps)
-
-end = andgat(letur, 2, 3, end, gps, start)
-end = exclugat(letur, 5, 7, end, gps, start, 1)
-
-end = tobegat(letur, 1, 5, end, gps, start, 0)
-#'''
-
-#'''
-x = 4
-for z in range(x):
-    end = makleen(letur, f'A{z}', end, gps)
-
-end = exclugat(letur, 0, 1, end, gps, start)
-end = andgat(letur, 0, 1, end, gps, start)
-
-end = exclugat(letur, 2, 3, end, gps, start)
-end = andgat(letur, 2, 3, end, gps, start)
-end = exclugat(letur, x + 1, x + 2, end, gps, start)
-end = andgat(letur, x + 1, x + 2, end, gps, start)
-
-end = exclugat(letur, 4, 3, end, gps, start)
-end = andgat(letur, 2, 3, end, gps, start)
-end = exclugat(letur, x + 1, x + 2, end, gps, start)
-end = andgat(letur, x + 1, x + 2, end, gps, start)
-
-end = exclugat(letur, 2, 3, end, gps, start)
-end = andgat(letur, 2, 3, end, gps, start)
-end = exclugat(letur, x + 1, x + 2, end, gps, start)
-end = andgat(letur, x + 1, x + 2, end, gps, start)
-
-
-#'''
-
-letur.setheading(0)
-letur.goto(start[0] - 30, start[1] + 10)
-input("W ")
